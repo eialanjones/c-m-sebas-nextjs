ae2d073ce41c004af45e1c24f08c821a6d2a9bfa
Alan Jones Rios <alan.jones.rios@gmail.com>
2024-11-25 16:45:06 -0300

    🔧 chore: ajustes na configuração do Next.js •⁠  ⁠desativado a ignorância de erros de build do TypeScript •⁠  ⁠removido espaço em branco desnecessário no webpack

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
-
+    
     return config;
   }
 };
